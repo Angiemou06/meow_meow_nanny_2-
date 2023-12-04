fb10016b60f61e9fe31894c5f052f17e1b2cf0ce
angie <annieaa61723@gmail.com>
2023-12-05 05:50:17 +0800

    close debug mode

--- app.py
+++ app.py
@@ -720,5 +720,5 @@ def orderCheck():
 
 if __name__ == '__main__':
     CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
-    socketio.run(app,host="0.0.0.0",port=3000,debug=True)
+    socketio.run(app,host="0.0.0.0",port=3000,debug=False)
 
